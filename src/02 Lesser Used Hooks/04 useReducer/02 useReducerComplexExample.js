ba2562aee4e269b927ea6e2ce1b8dba0293c8531
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-02-27 03:08:44 +0500

    useState repalced by useRef to avoid unnecessary renders

--- src/02 Lesser Used Hooks/04 useReducer/02 useReducerComplexExample.js
+++ src/02 Lesser Used Hooks/04 useReducer/02 useReducerComplexExample.js
@@ -1,27 +1,34 @@
-import { useReducer, useState } from "react";
+import { useReducer, useRef } from "react";
 import { reducer, ACTIONS } from "./reducer2";
 import Todo from "./Todo";
 
 const UseReducerComplexExample = () => {
   const [todos, dispatch] = useReducer(reducer, []);
-  const [name, setName] = useState("");
+
+  const inputRef = useRef("");
 
   console.log("Main Component Rerendering");
 
   function handleSubmit(e) {
     e.preventDefault();
-    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
-    setName("");
+
+    console.log(inputRef.current.value);
+
+    dispatch({
+      type: ACTIONS.ADD_TODO,
+      payload: { name: inputRef.current.value },
+    });
+
+    inputRef.current.value = "";
   }
 
   return (
     <>
+      <h1>Todo's App</h1>
+
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+        <input ref={inputRef} type="text" />
+        <button type="submit">Submit</button>
       </form>
 
       {todos.map((todo) => {
